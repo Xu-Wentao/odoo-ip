e42bc036ec5bbbc931d9e0ec281b788ea3599e2f
xuwentao <xuwentao@bjdvt.com>
2018-02-27 14:47:06 +0800

    修改title

--- addons/web/static/src/js/abstract_web_client.js
+++ addons/web/static/src/js/abstract_web_client.js
@@ -51,7 +51,8 @@ var WebClient = Widget.extend({
         this._current_state = null;
         this.menu_dm = new utils.DropMisordered();
         this.action_mutex = new utils.Mutex();
-        this.set('title_part', {"zopenerp": "Odoo"});
+        //xuwentao
+        this.set('title_part', {"zopenerp": ""});
     },
     start: function() {
         var self = this;
